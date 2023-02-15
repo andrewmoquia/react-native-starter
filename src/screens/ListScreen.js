e9b478f5af71d6d45714c670a6688f0d9af5dfab
andrewmoquia <andrewmoquia09@gmail.com>
2023-02-15 11:17:49 +0800

    feat: use flatlist element to display array of data

--- src/screens/ListScreen.js
+++ src/screens/ListScreen.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 const ListScreen = () => {
+
+    const pets = [
+        {
+            id: '1',
+            name: 'Bruno',
+            color: 'Brown',
+        },
+        {
+            id: '2',
+            name: 'Blacky',
+            color: 'Black',
+        },
+        {
+            id: '3',
+            name: 'Reddy',
+            color: 'Red',
+        },
+    ];
+
     return (
         <View>
-            <Text style={styles.textStyle}>Hellow!</Text>
+            <Text style={ styles.textStyle }>My Dogs List</Text>
+            <FlatList 
+                keyExtractor={( item ) => { item.id }}
+                data={ pets } 
+                renderItem={({ item }) => {
+                    return <Text>{ item.name }</Text>;
+                }}
+            />
         </View>
     )
 };
