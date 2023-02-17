aad0e9ecd9fb220dc42c97182322161b3db10815
andrewmoquia <andrewmoquia09@gmail.com>
2023-02-17 12:08:57 +0800

    feat: generate random color func

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 const ColorScreen = () => {
+
+    const defaultColorsVal = [];
+
+    const [colors, setColors] = useState(defaultColorsVal);
+
+    const randomRGB = () => {
+        const red = Math.floor(Math.random() * 256);
+        const green = Math.floor(Math.random() * 256);
+        const blue = Math.floor(Math.random() * 256);
+        return `rgba(${red}, ${green}, ${blue}, 1)`
+    };
+
+    const handleAddColorAction = () => setColors([...colors, randomRGB()]);
+
     return (
         <View>
             <Text style={styles.textStyle}>Color Screen!</Text>
-            <TouchableOpacity>
+
+            <TouchableOpacity onPress={() => handleAddColorAction()}>
                 <Text>Add a color!</Text>
             </TouchableOpacity>
+
+            <View style={{ height: 100, width: 100, backgroundColor: randomRGB() }} />
         </View>
     );
 };
+
+
 
 const styles = StyleSheet.create({
     textStyle: {
