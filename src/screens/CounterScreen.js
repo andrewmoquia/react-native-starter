066a04eac48e723de9dd6ec935270f3676f34611
andrewmoquia <andrewmoquia09@gmail.com>
2023-02-17 11:28:11 +0800

    feat: useState in action

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,10 +1,37 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 const CounterScreen = () => {
+
+    const defaultCounterVal = 0;
+
+    const [counter, setCounter] = useState(defaultCounterVal);
+
+    const handleCounterAction = (action) => {
+        switch (action) {
+            case 'increment':
+                setCounter(counter + 1);
+                break;
+            case 'decrement':
+                setCounter(counter - 1);
+                break;
+            default:
+                setCounter(counter);
+        }
+    };
+
     return (
         <View>
             <Text style={styles.textStyle}>Counter Screen!</Text>
+
+            <TouchableOpacity onPress={() => handleCounterAction('increment')}>
+                <Text>Increase</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => handleCounterAction('decrement')}>
+                <Text>Decrease</Text>
+            </TouchableOpacity>
+
+            <Text>Count: {counter}</Text>
         </View>
     );
 };
