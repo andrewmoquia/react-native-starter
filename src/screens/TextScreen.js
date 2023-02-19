9af4bac9cc6b546a5ddcee60656b1849c64a8615
andrewmoquia <andrewmoquia09@gmail.com>
2023-02-19 11:25:40 +0800

    feat: password validity and show/hide

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Text, TextInput } from "react-native";
+import { View, StyleSheet, Text, TextInput, TouchableOpacity } from "react-native";
 
 const TextScreen = () => {
 
     const defaultNameValue = '';
+    const defaultIsPasswordShow = true;
+    const defaultPassswordValid = false;
 
     const [name, setName] = useState(defaultNameValue);
+    const [isPasswordShow, showPassword] = useState(defaultIsPasswordShow);
+    const [isPasswordValid, setIsPasswordValid] = useState(defaultPassswordValid);
 
-    const handleNameInputAction = (value) => {
-        setName(value);
-    };
+    const handleNameInputAction = (value) => setName(value);
+    const handleEnterPasswordAction = (value) => value.length > 5 ? setIsPasswordValid(true) : setIsPasswordValid(false);
+    const handleShowPasswordAction = () => showPassword(!isPasswordShow);
 
     return (
         <View>
@@ -24,6 +28,25 @@ const TextScreen = () => {
             />
 
             <Text>Your name: {name}</Text>
+
+            <Text style={styles.textStyle}>Enter Password!</Text>
+
+            <TextInput
+                style={styles.inputStyle}
+                autoCapitalize="none"
+                autoCorrect={false}
+                autoComplete="off"
+                secureTextEntry={isPasswordShow}
+                onChangeText={(value) => handleEnterPasswordAction(value)}
+            />
+
+            <TouchableOpacity
+                onPress={handleShowPasswordAction}
+            >
+                <Text>Show Password!</Text>
+            </TouchableOpacity>
+
+            {!isPasswordValid && <Text>Password must be longer than 5 characters!</Text>}
         </View>
     );
 };
@@ -31,6 +54,7 @@ const TextScreen = () => {
 const styles = StyleSheet.create({
     textStyle: {
         fontSize: 24,
+        marginTop: 10,
     },
     inputStyle: {
         margin: 15,
