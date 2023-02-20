5ab564e0b3fa95b56953cd785778ae64cc67f0b8
andrewmoquia <andrewmoquia09@gmail.com>
2023-02-20 12:16:54 +0800

    feat: add flex properties

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -3,8 +3,20 @@ import { View, StyleSheet, Text } from "react-native";
 
 const BoxScreen = () => {
     return (
-        <View style={styles.viewStyle}>
-            <Text style={styles.textStyle}>Box Screen!</Text>
+        <View>
+            <Text style={styles.textStyle}>Box with padding, margin, and width!</Text>
+            <View style={styles.boxViewStyle}>
+                <Text style={styles.boxViewTextStyle}>Box Screen!</Text>
+            </View>
+
+            <Text style={styles.textStyle}>Flexbox!</Text>
+            <View style={styles.flexboxViewStyle}>
+                <Text style={styles.flexboxViewTextStyle}>Child #1</Text>
+                <Text style={styles.flexboxViewTextStyle}>Child #2</Text>
+                <Text style={styles.flexboxViewTextStyle}>Child #3</Text>
+                <Text style={styles.flexboxViewTextStyle}>Child #4</Text>
+                <Text style={styles.flexboxViewTextStyle}>Child #5</Text>
+            </View>
         </View>
     );
 };
@@ -12,16 +24,38 @@ const BoxScreen = () => {
 const styles = StyleSheet.create({
     textStyle: {
         fontSize: 24,
+        marginBottom: 10,
+        marginTop: 10,
+    },
+    boxViewTextStyle: {
+        fontSize: 24,
         borderColor: 'red',
         borderWidth: 10,
         padding: 20,
     },
-    viewStyle: {
+    boxViewStyle: {
         borderWidth: 1,
         borderColor: 'black',
         padding: 30,
         margin: 10,
-    }
+    },
+    flexboxViewStyle: {
+        borderColor: 'blue',
+        borderWidth: 2,
+        margin: 10,
+        padding: 10,
+        alignItems: 'center',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+    flexboxViewTextStyle: {
+        fontSize: 20,
+        borderWidth: 2,
+        borderColor: 'black',
+        margin: 2,
+        padding: 2,
+        paddingLeft: 5,
+    },
 });
 
 export default BoxScreen;
